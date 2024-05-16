4df65641dad697cd824492134562dbbed979476b
Marcos Hernan Parella <marcosparella2000@gmail.com>
2024-05-16 01:50:05 -0300

    cositas

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,49 @@
+(() => {
+  function getMovieById(id: string) {
+    console.log({ id });
+  }
+
+  function getMovieCastById(id: string) {
+    console.log({ id });
+  }
+
+  function getActorBioById(id: string) {
+    console.log({ id });
+  }
+
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+
+  function createMovie({ cast, description, rating, title }: Movie) {
+    console.log({ cast, description, rating, title });
+  }
+
+  function checkFullName(fullName: string) {
+    console.log({ fullName });
+
+    return true;
+  }
+
+  function createActor(birthdate: Date, fullName: string): boolean {
+    if (checkFullName(fullName)) return false;
+
+    console.log('Crear actor', birthdate);
+    return true;
+  }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
+})();
